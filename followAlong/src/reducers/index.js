699577888ae98445e080a96457a78992044a9068
Andy Bolos <andybolos@MacBook-Pro-2.local>
2021-07-20 21:27:07 -0600

    lecture work-in-progress

---
+++ followAlong/src/reducers/index.js
@@ -0,0 +1,28 @@
+export const intitalState = {
+    title: 'Hello earthlings!',
+    edting: false,
+    newTitleText: ''
+}
+
+
+// reducer is a function
+    // takes 2 args
+        // state
+        // action
+// has a switch
+    // the cases are what we are doing to our state
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case('SET_TITLE'): 
+            return state
+        case('SET_EDITING'): 
+            return state
+        case('SET_NEW_TITLE_TEXT'): 
+            return state
+        default: 
+            return state
+    }
+}
+
+export default reducer
